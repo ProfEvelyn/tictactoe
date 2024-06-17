e02f11094b2578085d78797755d68faa5c35950e
ProfEvelyn <157323617+ProfEvelyn@users.noreply.github.com>
2024-06-17 15:41:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def welcome(info_board):
 
 
 def printBoard(board):
+    '''
+    Prints the board to the screen
+    '''
     print(f"{board[0]} | {board[1]} | {board[2]} ")
     print(f"{board[3]} | {board[4]} | {board[5]} ")
     print(f"{board[6]} | {board[7]} | {board[8]} ")
